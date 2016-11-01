dce7f497a956ddffdf9ca7a24b6ee04eacd2e306
VenkateshVarmaP <varmapv1994@gmail.com>
2016-11-02 00:13:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -38,12 +38,21 @@ var articles = {
     }
 };
 
+function createTemplate(data){
+    var title = data.title;
+    var heading = data.heading;
+    var date = data.date;
+    var content = data.content;
+    
+    
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function (req, res){
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.send(articles[article-one]);
 });
 
 app.get('/article-two', function (req, res){
